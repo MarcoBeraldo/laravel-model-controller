5b2b525765fb916748468feebb09906f069bae33
MarcoBeraldo <beraldomar@gmail.com>
2022-09-20 15:08:33 +0200

    exercise completed

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('main-content')
+    <div class="container mt-5">
+        <h1 class="text-white">Benvenuto!</h1>
+    </div>
+@endsection
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <nav class="navbar bg-dark navbar-dark">
+        <div class="container-fluid">
+            <div>
+                <a class="navbar-brand" href="{{ url('/') }}">Home</a>
+                <a class="navbar-brand" href="{{ route('movies.index') }}">Movies</a>
+            </div>
+        </div>
+      </nav>
+</header>
